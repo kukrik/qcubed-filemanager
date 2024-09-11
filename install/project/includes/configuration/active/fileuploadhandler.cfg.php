68176b0e4cddbb3fe5ce00300e0e6c731aa0ec67
kukrik <tiit.papp@gmail.com>
2024-09-11 18:30:39 +0300

    Removed for upgrade

--- install/project/includes/configuration/active/fileuploadhandler.cfg.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * fileupload defines
- */
-
-define('QCUBED_FILEUPLOAD_HANDLER_URL', dirname(QCUBED_PROJECT_DIR) . '/vendor/kukrik/qcubed-filemanager');
-define('QCUBED_FILEUPLOAD_HANDLER_ASSETS_URL', dirname(QCUBED_BASE_URL) . '/kukrik/qcubed-filemanager/assets');
